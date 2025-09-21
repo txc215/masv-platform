d9c17ed16510c6104461c31fcaf91cc9e313eb87
txc215 <chenflysong@gmail.com>
2025-09-20 22:01:20 -0400

    Updated Dockerfile to fix issue on github aciton and update docker-compose.ci.yml for simplized and rm old main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # main.py
 import pandas as pd
-from simulator_core.algorithms.ekf import run_ekf
+# from simulator_core.algorithms.ekf import run_ekf
 
 if __name__ == "__main__":
     data_path = "data/sample_inputs/dummy_gnss_imu.csv"
     output_path = "data/sample_results/ekf_output.csv"
 
     df = pd.read_csv(data_path)
-    result_df = run_ekf(df)
-    result_df.to_csv(output_path, index=False)
-    print(f"EKF completed. Results saved to {output_path}")
+    # result_df = run_ekf(df)
+    # result_df.to_csv(output_path, index=False)
+    # print(f"EKF completed. Results saved to {output_path}")
